27d5ce6def0b660dcd4e5b1962ed8eb66ceeee49
elin <elin.speth@alumni.fh-aachen.de>
2024-05-29 23:46:45 +0200

    added todo

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -7,6 +7,7 @@
         // TODO: Refactor chessmen classes
         // TODO: GameOver and Patt Property in Game class or in IPlayer?
         // TODO: Methoden zum Initialisieren des Schachbretts refactoren?
+        // TODO: Eigentlich ist es doppelt die Farbe sowohl in Player als auch in Chessman zu speichern, weil die Figuren sowieso in der Chessmen Liste gespeichert sind.
         public static void Main(string[] args)
         {
             var game = new Game();
