74296026cf44512818680abe2280e13d1b6aee17
elin <elin.speth@alumni.fh-aachen.de>
2024-05-29 10:42:39 +0200

    refactoring

--- Chess/Game.cs
+++ Chess/Game.cs
@@ -34,7 +34,7 @@ namespace Chess
         {
             var player = ReadDesiredColor();
             Board = CreateBoard();
-            InitializeChessmen();
+            InitializeChessmen(); // TODO: sollte die Methode lieber am Ende von CreateBoard() aufgerufen werden?
         }
 
         private void InitializeChessmen()
@@ -67,42 +67,22 @@ namespace Chess
         private void InitializeKnights(Board board)
         {
             // add white rooks
-            var knight1 = new Knight("White", 1, 0);
-            White.Chessmen.Add(knight1);
-            board[1, 0].Owner = knight1;
-
-            var knight2 = new Knight("White", 6, 0);
-            White.Chessmen.Add(knight2);
-            board[6, 0].Owner = knight2;
+            board[1, 0].Owner = new Knight("White", 1, 0);
+            board[6, 0].Owner = new Knight("White", 6, 0);
 
             // add black rooks
-            var knight3 = new Knight("Black", 1, 7);
-            White.Chessmen.Add(knight3);
-            board[1, 7].Owner = knight3;
-
-            var knight4 = new Knight("Black", 6, 7);
-            White.Chessmen.Add(knight4);
-            board[6, 7].Owner = knight4;
+            board[1, 7].Owner = new Knight("Black", 1, 7);
+            board[6, 7].Owner = new Knight("Black", 6, 7);
         }
         private void InitializeRooks(Board board)
         {
             // add white rooks
-            var rook1 = new Rook("White", 0, 0);
-            White.Chessmen.Add(rook1);
-            board[0, 0].Owner = rook1;
-
-            var rook2 = new Rook("White", 7, 0);
-            White.Chessmen.Add(rook2);
-            board[7, 0].Owner = rook2;
+            board[0, 0].Owner = new Rook("White", 0, 0);
+            board[7, 0].Owner = new Rook("White", 7, 0);
 
             // add black rooks
-            var rook3 = new Rook("Black", 0, 7);
-            White.Chessmen.Add(rook3);
-            board[0, 7].Owner = rook3;
-
-            var rook4 = new Rook("Black", 7, 7);
-            White.Chessmen.Add(rook4);
-            board[7, 7].Owner = rook4;
+            board[0, 7].Owner = new Rook("Black", 0, 7);
+            board[7, 7].Owner = new Rook("Black", 7, 7);
         }
 
         private void InitializePawns(Board board)
@@ -110,17 +90,13 @@ namespace Chess
             // add white pawns
             for (var i = 0; i < 7; i++)
             {
-                var pawn = new Pawn("White", i, 1);
-                White.Chessmen.Add(pawn);
-                board[i, 1].Owner = pawn;
+                board[i, 1].Owner = new Pawn("White", i, 1);
             }
 
             // add black pawns
             for (var i = 0; i < 7; i++)
             {
-                var pawn = new Pawn("Black", i, 6);
-                Black.Chessmen.Add(pawn);
-                board[i, 6].Owner = pawn;
+                board[i, 6].Owner = new Pawn("Black", i, 6);
             }
         }
 
