7f277211bf352d5586d896e8896da220e479e0b3
elin <elin.speth@alumni.fh-aachen.de>
2024-05-29 08:10:30 +0200

    added todo and removed unused using

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
-
-namespace Chess
+﻿namespace Chess
 {
     public class Program
     {
         // TODO: enum für die Farben
         // TODO: use point instead of X and Y?
+        // TODO: Refactor chessmen classes
         public static void Main(string[] args)
         {
             var game = new Game();
